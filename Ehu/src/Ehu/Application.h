812ec472a7cb22e37122ae53eec0435d287c3045
gua8gua <1341888475@qq.com>
2025-10-21 11:31:34 +0800

    cmake

--- Ehu/src/Ehu/Application.h
+++ Ehu/src/Ehu/Application.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Core.h"
-#include "Events/EventHandler.h"
+#include "EventHandler.h"
 
 namespace Ehu
 {
---
+++ Ehu/src/Ehu/EventHandler.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Events/Event.h"
+
+#include <functional>
+#include <unordered_map>
+
+namespace Ehu {
+    class EventHandler {
+    public:
+        static void RegisterHandler(EventType type, std::function<void(Event&)> handler) {
+            eventHandlers[type].push_back(handler);
+        }
+
+        static void DispatchEvent(Event& event) {
+            auto& handlers = eventHandlers[event.GetEventType()];
+            for (auto& handler : handlers) {
+                handler(event);
+            }
+        }
+
+    private:
+        static std::unordered_map<EventType, std::vector<std::function<void(Event&)>>> eventHandlers;
+    };
+}
+
